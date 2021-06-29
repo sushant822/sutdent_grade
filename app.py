1707012c327acea915aeb3f45046c4aaa6e7fdb6
Sushant Deshpande <srdyyc@gmail.com>
2021-06-29 08:24:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,56 +17,6 @@ app = Flask(__name__)
 
 db = SQLAlchemy()
 
-# connect to the db
-con = psycopg2.connect(
-            host="ec2-34-238-26-109.compute-1.amazonaws.com",
-            database="d1jfbocoh8eqgo",
-            user="hexbstnnfbqjwk",
-            password="13f84410f3efc3df8c053dd3452713a5e63bafe4a7dad292256ae514193d2784"
-)
-
-
-
-def calgary_data_fun():
-    cur = con.cursor()
-
-    # execute query
-    # cur.execute("SELECT json_agg(t) FROM (SELECT price, bed, full_bath, half_bath, property_area, years_old, distance_downtown, lot_size, basement, garage, walk_score, bike_score, transit_score, house, condo, townhouse FROM listings)t")
-
-    cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000 AND s.walk_score > 5 AND s.bike_score > 0 AND s.transit_score > 0)t")
-
-    calgary_data = cur.fetchall()
-    # print(calgary_data)
-
-    # close cursor
-    #cur.close()
-
-    # close the connection
-    #con.close()
-
-    return calgary_data
-
-def bar_fun():
-    cur = con.cursor()
-
-    # execute query
-    # cur.execute("SELECT json_agg(t) FROM (SELECT price, bed, full_bath, half_bath, property_area, years_old, distance_downtown, lot_size, basement, garage, walk_score, bike_score, transit_score, house, condo, townhouse FROM listings)t")
-
-    # cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary AS cl JOIN score AS s ON cl.postal_code = s.postal_code JOIN coordinates AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000)t")
-
-    cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000 AND cl.property_type IS NOT null)t")
-
-    bar_data = cur.fetchall()
-    # print(calgary_data)
-
-    # close cursor
-    #cur.close()
-
-    # close the connection
-    #con.close()
-
-    return bar_data
-
 @app.route("/")
 def home():
 
@@ -91,58 +41,6 @@ def home():
 
     # Return template and data
     return render_template("index.html", calgary=[i for i in calgary_data])
-
-
-
-@app.route("/jsonified")
-def calgary_data():
-    """Return the Calgary data as json"""
-    calgary = calgary_data_fun()
-    # calgary = calgary[:15]
-    return jsonify(calgary)
-
-
-@app.route("/bardata")
-def bar_data():
-    bar = bar_fun()
-    return jsonify(bar)
-
-
-@app.route("/viz")
-def viz():
-    return render_template("viz.html")
-
-@app.route("/scatter")
-def scatter():
-    return render_template("scatter.html")
-
-@app.route("/bar")
-def bar():
-    return render_template("bar.html")
-
-@app.route("/data")
-def data():
-    # create cursor
-    cur = con.cursor()
-
-    # execute query
-    # cur.execute("SELECT price, bed, full_bath, half_bath, property_area, years_old, distance_downtown, lot_size, basement, garage, walk_score, bike_score, transit_score, house, condo, townhouse FROM listings")
-
-    # cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary AS cl JOIN score AS s ON cl.postal_code = s.postal_code JOIN coordinates AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000)t")
-
-    cur.execute("SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000")
-    
-    calgary_data = cur.fetchall()
-    # print(calgary_data)
-
-    # close cursor
-    #cur.close()
-
-    # close the connection
-    #con.close()
-
-    # Return template and data
-    return render_template("data.html", calgary=[i for i in calgary_data])
 
 @app.route("/houseprice")
 def house_price():
