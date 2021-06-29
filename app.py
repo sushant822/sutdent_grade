24b06b46c55f301e2b1ee81f58bf72c5d35f8a71
Sushant Deshpande <srdyyc@gmail.com>
2021-06-29 10:40:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, url_for
-from flask_sqlalchemy import SQLAlchemy
-import psycopg2
+#from flask_sqlalchemy import SQLAlchemy
+#import psycopg2
 from flask import request
 import joblib
 import pgeocode
