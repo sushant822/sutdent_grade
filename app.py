0c8f827b3cb6c7a806e2c5f4859b0baf0fe27aa2
Sushant Deshpande <srdyyc@gmail.com>
2021-06-29 10:47:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, url_for
 #from flask_sqlalchemy import SQLAlchemy
-import psycopg2
+#import psycopg2
 from flask import request
 import joblib
 import pgeocode
@@ -14,33 +14,6 @@ import re
 #from tensorflow import keras
 
 app = Flask(__name__)
-
-#db = SQLAlchemy()
-
-@app.route("/")
-def home():
-
-    # create cursor
-    cur = con.cursor()
-
-    # execute query
-    # cur.execute("SELECT json_agg(t) FROM (SELECT price, bed, full_bath, half_bath, property_area, years_old, distance_downtown, lot_size, basement, garage, walk_score, bike_score, transit_score, house, condo, townhouse FROM listings)t")
-
-    # cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary AS cl JOIN score AS s ON cl.postal_code = s.postal_code JOIN coordinates AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000)t")
-
-    cur.execute("SELECT json_agg(t) FROM (SELECT cl.price, cl.address, cl.postal_code, cl.bed, cl.full_bath, cl.half_bath, cl.property_area, cl.property_type, s.walk_score, s.bike_score, s.transit_score, coord.lat, coord.long FROM calgary_df AS cl JOIN score_df AS s ON cl.postal_code = s.postal_code JOIN coordinates_df AS coord ON s.postal_code = coord.postal_codes WHERE cl.price < 1000000 AND cl.property_area < 4000)t")
-    
-    calgary_data = cur.fetchall()
-    # print(calgary_data)
-
-    # close cursor
-    #cur.close()
-
-    # close the connection
-    #con.close()
-
-    # Return template and data
-    return render_template("index.html", calgary=[i for i in calgary_data])
 
 @app.route("/houseprice")
 def house_price():
