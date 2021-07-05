c45def9ba0dab86e3e9a6e413cd886d6f1471ca1
Sushant Deshpande <srdyyc@gmail.com>
2021-07-05 11:45:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -30,36 +30,38 @@ def house_price():
     
     return render_template("houseprice.html")
 
-@app.route("/houseprice",methods=['POST'])
-def getvalues():
-    studytime = request.form['studytime']
-    failures = request.form['failures']
-    freetime = request.form['freetime']
-    absences = request.form['absences']
-    health = request.form['health']
-    grade_1 = request.form['grade_1']
-    grade_2 = request.form['grade_2']
+@app.route("/houseprice",methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        
+        studytime = request.form['studytime']
+        failures = request.form['failures']
+        freetime = request.form['freetime']
+        absences = request.form['absences']
+        health = request.form['health']
+        grade_1 = request.form['grade_1']
+        grade_2 = request.form['grade_2']
 
-    ###### Convert to numeric ######
-    studytime = int(studytime)
-    failures = int(failures)
-    freetime = int(freetime)
-    absences = int(absences)
-    health = int(health)
-    grade_1 = int(grade_1)
-    grade_2 = int(grade_2)
+        ###### Convert to numeric ######
+        studytime = int(studytime)
+        failures = int(failures)
+        freetime = int(freetime)
+        absences = int(absences)
+        health = int(health)
+        grade_1 = int(grade_1)
+        grade_2 = int(grade_2)
 
-    ###### ML Model ######
-    filename = 'data/LogisticRegression.sav'
+        ###### ML Model ######
+        filename = 'data/LogisticRegression.sav'
 
-    joblib_LR_model = joblib.load(filename)
+        joblib_LR_model = joblib.load(filename)
 
-    test_data = [[studytime, failures, freetime, absences, health, grade_1, grade_2]]
+        test_data = [[studytime, failures, freetime, absences, health, grade_1, grade_2]]
 
-    Ypredict_full = joblib_LR_model.predict(test_data)
-    Ypredict = np.round_(Ypredict_full, 2)
+        Ypredict_full = joblib_LR_model.predict(test_data)
+        Ypredict = np.round_(Ypredict_full, 2)
 
-    return render_template("houseprice.html", Ypredict=[Ypredict])
+        return render_template("houseprice.html", Ypredict=[Ypredict])
     ####### END #######
 
 if __name__ == "__main__":
