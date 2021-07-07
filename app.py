977b6c7a5bf167c5ced49df504a732e953c6515f
Sushant Deshpande <srdyyc@gmail.com>
2021-07-07 09:58:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -33,16 +33,18 @@ def getvalues():
     grade_2 = int(grade_2)
 
     ###### ML Model ######
-    filename = 'StudentGrade.sav'
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    filename = open(os.path.join(__location__, 'StudentGrade.sav'), errors="ignore")
+    #filename = 'StudentGrade.sav'
 
-    joblib_LR_model = joblib.load(filename)
+    joblib_model = joblib.load(filename)
 
     test_data = [[studytime, failures, freetime, absences, health, grade_1, grade_2]]
 
-    Ypredict_full = joblib_LR_model.predict(test_data)
+    Ypredict_full = joblib_model.predict(test_data)
     Ypredict = np.round_(Ypredict_full, 2)
 
-    return render_template("index.html", Ypredict=[Ypredict])
+    return render_template("houseprice.html", Ypredict=[Ypredict])
     ####### END #######
 
 if __name__ == "__main__":
